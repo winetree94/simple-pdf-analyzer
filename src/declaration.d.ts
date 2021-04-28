45095c822fe2ab8dc5c702fc49322beed721c910
winetree94 <winetree94@outlook.com>
2021-04-28 14:27:38 +0000

    feat: complete internal logic

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -7,3 +7,23 @@ declare module '*.scss' {
   const url: string;
   export = url;
 }
+
+declare module 'pdfjs-dist/lib/core/pdf_manager' {
+  const module: { LocalPdfManager: any };
+  export = module;
+}
+
+declare module 'pdfjs-dist/lib/core/primitives' {
+  const module: {
+    Ref: new (num: number, gen: number) => any;
+    RefSet: new (parent?: any) => any;
+    isDict: (obj: any) => boolean;
+    isRef: (obj: any) => boolean;
+    isStream: (obj: any) => boolean;
+    isName: (obj: any) => boolean;
+    isNum: (obj: any) => boolean;
+    isBool: (obj: any) => boolean;
+    isString: (obj: any) => boolean;
+  };
+  export = module;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,116 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import './main.scss';
-import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist';
+import { LocalPdfManager } from 'pdfjs-dist/lib/core/pdf_manager';
+import {
+  isDict,
+  isStream,
+  isRef,
+  Ref,
+  isName,
+  RefSet,
+} from 'pdfjs-dist/lib/core/primitives';
 
-GlobalWorkerOptions.workerSrc = '../../build/webpack/pdf.worker.bundle.js';
+class Node {
+  public name: string;
+  public value: any;
+  public hasChild: boolean;
+  public type: ValueType;
+  private xref: any;
+
+  constructor(name: string, value: any, xref?: any) {
+    this.type = getPdfValueType(value);
+
+    if (this.type === ValueType.REF) {
+      this.value = xref.fetch(value);
+    } else {
+      this.value = value;
+    }
+
+    this.name = name;
+    this.xref = xref;
+
+    this.hasChild = [
+      ValueType.DICT,
+      ValueType.STREAM,
+      ValueType.ARRAY,
+    ].includes(this.type);
+  }
+
+  public getChildren(): Node[] {
+    if (this.hasChild) {
+      const children: Node[] = [];
+      switch (this.type) {
+        case ValueType.DICT:
+          Object.entries(this.value._map).forEach(([key, value]) =>
+            children.push(new Node(key, value, this.value.xref))
+          );
+          break;
+        case ValueType.STREAM:
+          Object.entries(this.value.dict._map).forEach(([key, value]) =>
+            children.push(new Node(key, value))
+          );
+          children.push(new Node('Stream', 'Contents'));
+          break;
+        case ValueType.ARRAY:
+          this.value.forEach((value: any, index: number) => {
+            children.push(new Node(index.toString(), value));
+          });
+          break;
+      }
+      return children;
+    } else {
+      throw new Error('This node not expandable');
+    }
+  }
+}
 
 export const root = document.getElementById('root') as HTMLDivElement;
+
+export enum ValueType {
+  DICT = 'dict',
+  STREAM = 'stream',
+  ARRAY = 'array',
+  REF = 'ref',
+  NAME = 'name',
+  NUM = 'num',
+  BOOL = 'bool',
+  STRING = 'string',
+}
+
+function getPdfValueType(value: any): ValueType {
+  if (isDict(value)) {
+    return ValueType.DICT;
+  } else if (isStream(value)) {
+    return ValueType.STREAM;
+  } else if (Array.isArray(value)) {
+    return ValueType.ARRAY;
+  } else if (isRef(value)) {
+    return ValueType.REF;
+  } else if (isName(value)) {
+    return ValueType.NAME;
+  } else if (typeof value === 'boolean') {
+    return ValueType.BOOL;
+  } else if (typeof value === 'string') {
+    return ValueType.STRING;
+  } else if (typeof value === 'number') {
+    return ValueType.NUM;
+  } else {
+    console.log(value);
+    throw new Error('Unresolvable value type!');
+  }
+}
+
+function parseArrayBuffer(data: Uint8Array): any {
+  const pdfManager = new LocalPdfManager(null, data).pdfDocument;
+  pdfManager.parseStartXRef();
+  pdfManager.parse();
+  const tree = pdfManager.xref.trailer;
+  return tree;
+}
 
 function isPdf(file: File): boolean {
   const splitted = file.name.split('.');
@@ -41,21 +148,10 @@ root.addEventListener('drop', (e) => {
     const file = e.dataTransfer.files[0];
 
     fileToArrayBuffer(file)
-      .then(async (buffer) => {
-        const document = await getDocument(buffer).promise;
-        console.log(document);
-
-        const fields = await document.getFieldObjects();
-        console.log(fields);
-
-        // const responses = await Promise.all(
-        //   Array.from(new Array(document.numPages)).map(async (n, index) => {
-        //     const page = await document.getPage(index + 1);
-        //     console.log(page);
-        //   })
-        // );
-
-        console.log(document);
+      .then((buffer) => {
+        const dict = parseArrayBuffer(buffer);
+        const node = new Node('root', dict, dict.xref);
+        console.log(node);
       })
       .catch((e) => {
         console.error(e);
